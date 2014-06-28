17e40e30e44a7c56246010e760b0417da0d3dcfb
Noah <nms277@gmail.com>
2014-06-28 18:53:21 +0900

    added AES crypt/decrypt

--- crypto.go
+++ crypto.go
@@ -1,10 +1,14 @@
 package utils
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
 	"crypto/hmac"
+	"crypto/rand"
 	"crypto/sha256"
 	"encoding/base64"
 	"fmt"
+	"io"
 	"strings"
 )
 
@@ -70,4 +74,65 @@ func VerifyHMAC(message string, macOfMessage []byte, key string) bool {
 	}
 	expectedMAC := mac.Sum(nil)
 	return hmac.Equal(macOfMessage, expectedMAC)
+}
+
+//////////////////////////////////////////////////////////////////////////
+//
+// encrypt string to base64 crypto using AES
+//
+//
+//////////////////////////////////////////////////////////////////////////
+func encryptAESCFB(key []byte, text string) string {
+
+	plaintext := []byte(text)
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err)
+	}
+
+	// The IV needs to be unique, but not secure. Therefore it's common to
+	// include it at the beginning of the ciphertext.
+	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
+	iv := ciphertext[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		panic(err)
+	}
+
+	stream := cipher.NewCFBEncrypter(block, iv)
+	stream.XORKeyStream(ciphertext[aes.BlockSize:], plaintext)
+
+	// convert to base64
+	return base64.URLEncoding.EncodeToString(ciphertext)
+}
+
+//////////////////////////////////////////////////////////////////////////
+//
+// decrypt from base64 to decrypted string
+//
+//
+//////////////////////////////////////////////////////////////////////////
+func decryptAESCFB(key []byte, cryptoText string) string {
+
+	ciphertext, _ := base64.URLEncoding.DecodeString(cryptoText)
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err)
+	}
+
+	// The IV needs to be unique, but not secure. Therefore it's common to
+	// include it at the beginning of the ciphertext.
+	if len(ciphertext) < aes.BlockSize {
+		panic("ciphertext too short")
+	}
+	iv := ciphertext[:aes.BlockSize]
+	ciphertext = ciphertext[aes.BlockSize:]
+
+	stream := cipher.NewCFBDecrypter(block, iv)
+
+	// XORKeyStream can work in-place if the two arguments are the same.
+	stream.XORKeyStream(ciphertext, ciphertext)
+
+	return fmt.Sprintf("%s", ciphertext)
 }
